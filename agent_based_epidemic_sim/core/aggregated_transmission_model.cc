31bb938f25f94f8555bb5e1c40b680d68d92af6a
groeney <groeney@google.com>
2020-07-21 13:09:13 -0700

    Adding hazard computation for an Exposure which is a sum of hazards of micro exposures.
    PiperOrigin-RevId: 322425963

--- agent_based_epidemic_sim/core/aggregated_transmission_model.cc
+++ agent_based_epidemic_sim/core/aggregated_transmission_model.cc
@@ -15,12 +15,55 @@
 #include "agent_based_epidemic_sim/core/aggregated_transmission_model.h"
 
 #include "absl/random/distributions.h"
+#include "absl/time/time.h"
+#include "agent_based_epidemic_sim/core/constants.h"
 
 namespace abesim {
 namespace {
 // TODO: Move into the visit message about the visiting agent.
 constexpr float kSusceptibility = 1;
 constexpr double kEpsilon = 1e-8;
+
+// Used as a constant when computing a distance to infection probability.
+// Details in (broken link).
+constexpr float kDistanceInfectionProbabilityS = 1.5;
+
+// Used as a constant when computing a distance to infection probability.
+// Details in (broken link).
+constexpr float kDistanceInfectionProbabilityB = 6.6;
+
+constexpr std::array<float, kNumberMicroExposureBuckets> kIndexToMeters = {
+    0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5};
+
+float DistanceToInfectionProbability(float distance) {
+  // Infectiousness is defined as a sigmoid curve that starts at 100% at 0
+  // distance, is 50% at 4 meters, and is 0% at 10 meters. These values are
+  // estimated heuristically.
+  return 1 - 1 / (1 + std::exp(-kDistanceInfectionProbabilityS * distance +
+                               kDistanceInfectionProbabilityB));
+}
+
+// Returns distance in meters as a function of MicroExposure bucket index.
+float ExtractDistanceFromIndex(size_t index) { return kIndexToMeters[index]; }
+
+float ExposureHazard(const Exposure& exposure) {
+  float hazard = 0;
+
+  if (exposure.duration > absl::Minutes(1) &&
+      exposure.micro_exposure_counts == kEmptyMicroExposures) {
+    hazard = absl::ToInt64Minutes(exposure.duration);
+  }
+
+  for (size_t i = 0; i < exposure.micro_exposure_counts.size(); i++) {
+    const uint8 duration_minutes = exposure.micro_exposure_counts[i];
+    if (duration_minutes > 0) {
+      const uint8 distance = ExtractDistanceFromIndex(i);
+
+      hazard += DistanceToInfectionProbability(distance) * duration_minutes;
+    }
+  }
+  return hazard * exposure.infectivity * exposure.symptom_factor;
+}
 
 float ProbabilityExposureInfects(const Exposure& exposure,
                                  const float transmissibility) {
