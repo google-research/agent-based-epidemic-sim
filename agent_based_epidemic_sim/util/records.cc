bbb0ec67012fd4201a0ae458cb42c5a37e270322
Pandemic-Sim Team <noreply@google.com>
2022-02-16 15:00:04 -0800

    Internal change
    PiperOrigin-RevId: 429148404

--- agent_based_epidemic_sim/util/records.cc
+++ agent_based_epidemic_sim/util/records.cc
@@ -1,20 +1,15 @@
 #include "agent_based_epidemic_sim/util/records.h"
 
-#include <fcntl.h>
-
 namespace abesim {
-
-constexpr int kReadFlag = O_RDONLY;
-constexpr int kWriteFlag = O_CREAT | O_WRONLY;
 
 riegeli::RecordReader<RiegeliBytesSource> MakeRecordReader(
     absl::string_view filename) {
-  return riegeli::RecordReader(RiegeliBytesSource(filename, kReadFlag));
+  return riegeli::RecordReader(RiegeliBytesSource(filename));
 }
 riegeli::RecordWriter<RiegeliBytesSink> MakeRecordWriter(
     absl::string_view filename, const int parallelism) {
   return riegeli::RecordWriter(
-      RiegeliBytesSink(filename, kWriteFlag),
+      RiegeliBytesSink(filename),
       riegeli::RecordWriterBase::Options().set_parallelism(parallelism));
 }
 
